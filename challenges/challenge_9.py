2f7d32744cf104b188b470d9f4e24597251426a3
Lucas <lucasdfrancesca@gmail.com>
2022-07-07 23:02:28 -0300

    added try positive days challenge 9

--- challenges/challenge_9.py
+++ challenges/challenge_9.py
@@ -1,13 +1,18 @@
-
 import datetime
 
 
 def generateLastDaysPaths(date, days):
     date_ = datetime.datetime.strptime(date, '%Y%m%d')
-    
-    for day in range(days - 1, -1, -1):
-        d = date_ - datetime.timedelta(days=day)
-        year, month, day = d.year, d.strftime('%m'), d.day
-        print(f'https://importantdata@location/{year}/{month}/{day}/')
+    try:
+        if days < 0:
+            raise ValueError
+
+        for day in range(days - 1, -1, -1):
+            d = date_ - datetime.timedelta(days=day)
+            year, month, day = d.year, d.strftime('%m'), d.day
+            print(f'https://importantdata@location/{year}/{month}/{day}/')
+
+    except ValueError:
+        print("days have to positive")
 
 generateLastDaysPaths("20210410", 10)
